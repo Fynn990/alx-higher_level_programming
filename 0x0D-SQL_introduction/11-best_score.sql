f9c6b4be936a9768b9c52a61e4272b04348e4f6a
Fynn990 <fmb28465@gmail.com>
2023-08-15 23:51:38 -0700

    update

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+--list all records with score >= 10 in DESC order.
+
+SELECT 'score', 'name' 
+FROM 'second_table'
+WHERE 'score' >= 10
+ORDER BY 'score' DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- updates the score of Bob to 10 in the table second_table.
+
+UPDATE 'users' SET 'score' = 10
+WHERE 'id' = 3;
